95b60459cd21521c6fcc777f09d1dc40a13d2117
chylex <contact@chylex.com>
2021-11-17 03:27:33 +0100

    Customize progress bar theme

--- src/main/java/com/kagof/intellij/plugins/pokeprogress/Painter.java
+++ src/main/java/com/kagof/intellij/plugins/pokeprogress/Painter.java
@@ -1,16 +1,11 @@
 package com.kagof.intellij.plugins.pokeprogress;
 
+import com.intellij.ui.JBColor;
+import com.intellij.ui.scale.JBUIScale;
 import java.awt.Color;
 import java.awt.LinearGradientPaint;
 import java.awt.Paint;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import org.apache.commons.lang.ArrayUtils;
-
-import com.intellij.ui.JBColor;
-import com.intellij.ui.scale.JBUIScale;
 
 public final class Painter {
     private static final float ONE_HALF = 0.5f;
@@ -33,25 +28,22 @@ public final class Painter {
         final List<PokemonType> types = pokemon.getTypes();
         final int numColors = types.size();
         if (numColors == 1) {
-            return getPaintSingleType(types.get(0), startY, height);
+	        return types.get(0).getColorDark();
         }
 
-        final float numColorsReciprocal = 1f / (numColors - 1);
-        return new LinearGradientPaint(0,
-            (float) startY + JBUIScale.scale(2f),
-            0,
-            (float) startY + (float) height - JBUIScale.scale(2f),
-            ArrayUtils.toPrimitive(
-                IntStream.range(0, numColors).mapToObj(i -> numColorsReciprocal * i).toArray(Float[]::new)),
-            types.stream().map(PokemonType::getColor).collect(Collectors.toList()).toArray(new Color[] {}));
+        final float numColorsReciprocal = 1f / numColors;
+		final float[] fractions = new float[numColors * 2];
+		final Color[] colors = new Color[numColors * 2];
+	
+	    for (int i = 0; i < numColors; i++) {
+			fractions[i * 2] = i == 0 ? 0F : Math.nextUp(numColorsReciprocal * i);
+			fractions[i * 2 + 1] = i == numColors - 1 ? 1F : Math.nextDown(numColorsReciprocal * (i + 1));
+		    final Color color = types.get(i).getColorDark();
+		    colors[i * 2] = color;
+		    colors[i * 2 + 1] = color;
+	    }
+		
+        return new LinearGradientPaint(0, startY, 0, startY + height, fractions, colors);
     }
-
-    private static Paint getPaintSingleType(final PokemonType type, final int startY, final int height) {
-        return new LinearGradientPaint(0,
-            (float) startY + JBUIScale.scale(2f),
-            0,
-            (float) startY + (float) height - JBUIScale.scale(2f),
-            new float[] {0f, ONE_HALF, 1f},
-            new Color[] {type.getColorLight(), type.getColor(), type.getColorDark()});
-    }
+	
 }
